19c4fa0bdd5e02bc6d1327d4ab2ae0049a4dc6de
sokolenkov <s9379992g@gmail.com>
2021-09-22 00:09:22 +0300

    init commit

---
+++ siteConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: "Lydia Retouch",
+  shortName: "LR",
+  description: "Retouching is a modern art form for everyone",
+  url: "https://gatsby-lam.weensy.co", // Domain of your site without prefix!
+  prefix: "/",
+  author: "Lydia",
+  twitter: "",
+}
